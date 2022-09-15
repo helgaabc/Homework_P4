c0224e5591bb8145e05283097ddee1b79fe6d10c
elena.bondarchuk <e-bondarchuk@mail.ru>
2022-09-15 11:20:39 +0300

    upd

--- Task 25/Program.cs
+++ Task 25/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int result = a;
+for (int i = 1; i < b; i++)
+    result = result * a;
+Console.WriteLine(a + " в степени " + b + " = " + result);
