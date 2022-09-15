16f3b84f830f8f3b59b10fdabd4488b1876c20f7
elena.bondarchuk <e-bondarchuk@mail.ru>
2022-09-15 14:15:08 +0300

    task 29

---
+++ Task 29/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] array = new int[8];
+int i = 0;
+while (i < 8)
+{
+    Console.Write("Введите в общей сложности восемь чисел: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    i++;
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");
